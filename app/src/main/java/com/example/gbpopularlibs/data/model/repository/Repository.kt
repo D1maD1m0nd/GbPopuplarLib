152bf48ad0c829a680e242a6368f47c9ff998ba0
d1ma <d1m0hkrasav4ik@gmail.com>
2021-12-19 06:30:51 +0500

    rx first sample

--- app/src/main/java/com/example/gbpopularlibs/data/model/repository/Repository.kt
+++ app/src/main/java/com/example/gbpopularlibs/data/model/repository/Repository.kt
@@ -1,8 +1,9 @@
 package com.example.gbpopularlibs.data.model.repository
 
 import com.example.gbpopularlibs.data.model.User
+import io.reactivex.rxjava3.core.Observable
 
 interface Repository {
     fun generateUser(): List<User>
-    fun getUser(login: String, password: String): User?
+    fun getUser(login: String, password: String): Observable<User?>
 }
--- app/src/main/java/com/example/gbpopularlibs/data/model/repository/RepositoryImpl.kt
+++ app/src/main/java/com/example/gbpopularlibs/data/model/repository/RepositoryImpl.kt
@@ -3,6 +3,7 @@ package com.example.gbpopularlibs.data.model.repository
 import com.example.gbpopularlibs.data.model.User
 import java.util.*
 import kotlin.collections.ArrayList
+import io.reactivex.rxjava3.core.Observable
 
 class RepositoryImpl : Repository {
     private val users = mutableListOf(User("admin", "admin"), User("root", "root"))
@@ -20,8 +21,9 @@ class RepositoryImpl : Repository {
         return users
     }
 
-    override fun getUser(login: String, password: String): User? {
-        return users.findLast { login == it.login && password == it.password }
+    override fun getUser(login: String, password: String): Observable<User?> {
+        var user = users.findLast { login == it.login && password == it.password }
+        return Observable.just(user)
     }
 
     private companion object {

1f4d50bc39fe9ee5991cab47181e1c0e2a85ed76
d1111111maaaaaaa <contentOrderLayout.visibility = View.VISIBLE>
2021-12-27 03:51:29 +0300

    hw6

--- app/src/main/java/com/example/gbpopularlibs/data/model/repository/GitHubUserRepositoryImpl.kt
+++ app/src/main/java/com/example/gbpopularlibs/data/model/repository/GitHubUserRepositoryImpl.kt
@@ -1,13 +1,32 @@
 package com.example.gbpopularlibs.data.model.repository
 
+import com.example.gbpopularlibs.data.db.RoomFactory
 import com.example.gbpopularlibs.data.model.GitHubUser
 import com.example.gbpopularlibs.data.rest.GitHubApiImpl
 import io.reactivex.rxjava3.core.Single
 
 class GitHubUserRepositoryImpl : GitHubUserRepository {
     var apiService = GitHubApiImpl.getApiService()
+    private val roomDb = RoomFactory.create().userDao()
+    override fun getGitHubUser(login: String) : Single<GitHubUser>{
+        return roomDb.getUserByLogin(login).flatMap {
+            Single.just(it)
+        }
+    }
 
-    override fun getGitHubUser(login: String) = apiService.getUserByLogin(login)
-    override fun getGitHubUsers(): Single<List<GitHubUser>> = apiService.getUsers()
+    override fun getGitHubUsers(): Single<List<GitHubUser>> {
+        return roomDb.getAllUsers()
+            .flatMap {
+                if (it.isEmpty()) {
+                    apiService.getUsers()
+                        .map { resultFromServer ->
+                            roomDb.insertAll(resultFromServer)
+                            resultFromServer
+                        }
+                } else {
+                    Single.just(it)
+                }
+            }
+    }
 
 }
